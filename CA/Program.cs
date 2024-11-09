b4736130dbfcfc87883831c16f38fcdb4924ec16
maior <maior@DESKTOP-FHGLER9>
2024-11-09 16:12:07 +0200

    Добавьте файлы проекта.

---
+++ CA/Program.cs
@@ -0,0 +1,16 @@
+﻿try {
+   Console.WriteLine("Введите трехзначное число: ");
+   int originalNumber = Convert.ToInt32(Console.ReadLine());
+   if (originalNumber < 100 || originalNumber > 999) 
+    { 
+    Console.WriteLine("Число не является трехзначным.");
+                return;
+     }
+    int lastDigit = originalNumber % 10;
+     int firstTwoDigits = originalNumber / 10;
+      int newNumber = lastDigit * 100 + firstTwoDigits;
+    Console.WriteLine("Новое число: " + newNumber);
+   }
+catch {
+    Console.WriteLine("Введите правильное занчение:");
+}
